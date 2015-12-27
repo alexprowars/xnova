0da783814c6dbacda78ffc77d44a83c962c25a30
alexpro <alexprowars@gmail.com>
2015-12-27 20:05:01 +0300

    Фикс имен файлов №5

---
+++ app/controllers/fleet/StageZero.php
@@ -0,0 +1,116 @@
+<?php
+namespace App\Controllers\Fleet;
+
+use App\Controllers\FleetController;
+use App\Fleet;
+use App\Lang;
+
+class StageZero
+{
+	public function show (FleetController $controller)
+	{
+		if (!$controller->planet)
+			$controller->message(_getText('fl_noplanetrow'), _getText('fl_error'));
+
+		$MaxFlyingFleets = $controller->db->fetchColumn("SELECT COUNT(fleet_owner) AS `actcnt` FROM game_fleets WHERE `fleet_owner` = '" . $controller->user->id . "';");
+
+		$MaxExpedition = $controller->user->{$controller->game->resource[124]};
+		$ExpeditionEnCours = 0;
+		$EnvoiMaxExpedition = 0;
+
+		if ($MaxExpedition >= 1)
+		{
+			$ExpeditionEnCours = $controller->db->fetchColumn("SELECT COUNT(fleet_owner) AS `expedi` FROM game_fleets WHERE `fleet_owner` = '" . $controller->user->id . "' AND `fleet_mission` = '15';");
+			$EnvoiMaxExpedition = 1 + floor($MaxExpedition / 3);
+		}
+
+		$MaxFlottes = 1 + $controller->user->{$controller->game->resource[108]};
+		if ($controller->user->rpg_admiral > time())
+			$MaxFlottes += 2;
+
+		Lang::includeLang('fleet');
+
+		$galaxy = (isset($_GET['galaxy'])) ? intval($_GET['galaxy']) : 0;
+		$system = (isset($_GET['system'])) ? intval($_GET['system']) : 0;
+		$planet = (isset($_GET['planet'])) ? intval($_GET['planet']) : 0;
+		$planettype = (isset($_GET['planettype'])) ? intval($_GET['planettype']) : 0;
+		$target_mission = (isset($_GET['target_mission'])) ? intval($_GET['target_mission']) : 0;
+
+		if (!$galaxy)
+			$galaxy = $controller->planet->galaxy;
+
+		if (!$system)
+			$system = $controller->planet->system;
+
+		if (!$planet)
+			$planet = $controller->planet->planet;
+
+		if (!$planettype)
+			$planettype = 1;
+
+		$parse = array();
+		$parse['maxFlyingFleets'] = $MaxFlyingFleets;
+		$parse['maxFlottes'] = $MaxFlottes;
+		$parse['currentExpeditions'] = $ExpeditionEnCours;
+		$parse['maxExpeditions'] = $EnvoiMaxExpedition;
+		$parse['galaxy'] = $galaxy;
+		$parse['system'] = $system;
+		$parse['planet'] = $planet;
+		$parse['planettype'] = $planettype;
+		$parse['mission'] = $target_mission;
+
+		$fq = $controller->db->query("SELECT * FROM game_fleets WHERE fleet_owner=" . $controller->user->id . "");
+
+		$parse['fleets'] = array();
+
+		while ($f = $fq->fetch())
+		{
+			$f['fleet_count'] = 0;
+
+			$fleetArray = Fleet::unserializeFleet($f['fleet_array']);
+
+			foreach ($fleetArray as $fleetId => $fleetData)
+				$f['fleet_count'] += $fleetData['cnt'];
+
+			$f['fleet_array'] = $fleetArray;
+
+			$parse['fleets'][]= $f;
+		}
+
+		$parse['mission_text'] = '';
+
+		if ($target_mission > 0)
+			$parse['mission_text'] = ' для миссии "' . _getText('type_mission', $target_mission) . '"';
+		if (($system > 0 && $galaxy > 0 && $planet > 0) && ($galaxy != $controller->planet->galaxy || $system != $controller->planet->system || $planet != $controller->planet))
+			$parse['mission_text'] = ' на координаты [' . $galaxy . ':' . $system . ':' . $planet . ']';
+
+		$parse['ships'] = array();
+
+		foreach ($controller->game->reslist['fleet'] as $n => $i)
+		{
+			if ($controller->planet->{$controller->game->resource[$i]} > 0)
+			{
+				$ship = array
+				(
+					'id' => $i,
+					'count' => $controller->planet->{$controller->game->resource[$i]},
+					'consumption' => Fleet::GetShipConsumption($i, $controller->user),
+					'speed' => Fleet::GetFleetMaxSpeed("", $i, $controller->user)
+				);
+
+				if (isset($controller->user->{'fleet_' . $i}) && isset($controller->game->CombatCaps[$i]['power_consumption']) && $controller->game->CombatCaps[$i]['power_consumption'] > 0)
+					$ship['capacity'] = round($controller->game->CombatCaps[$i]['capacity'] * (1 + $controller->user->{'fleet_' . $i} * ($controller->game->CombatCaps[$i]['power_consumption'] / 100)));
+				else
+					$ship['capacity'] = $controller->game->CombatCaps[$i]['capacity'];
+
+				$parse['ships'][] = $ship;
+			}
+
+			$have_ships = true;
+		}
+
+		$controller->view->setVar('parse', $parse);
+		$controller->tag->setTitle(_getText('fl_title_0'));
+	}
+}
+?>
